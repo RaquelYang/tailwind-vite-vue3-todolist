1f87b7708d5fcae3050127bf49b58ee87a723525
RaquelYang <lorehmmoreh007@gmail.com>
2022-03-30 13:57:39 +0800

    add todolist
    todolist with vite-vue3-tailwind

---
+++ src/components/todo/TodoCreate.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+const emit = defineEmits();
+const todoBody = ref("");
+const todoBodyInput = ref(null);
+const todoCreate = () => {
+  if (!todoBody.value) {
+    return null;
+  } else {
+    emit("todoCreate", {
+      body: todoBody.value,
+      completed: false,
+    });
+    todoBody.value = "";
+    todoBodyInput.value.focus();
+  }
+};
+</script>
+<template>
+  <div class="flex border-b">
+    <input
+      @keydown.enter="todoCreate"
+      ref="todoBodyInput"
+      v-model="todoBody"
+      class="w-full px-4 py-2 focus:outline-none"
+      type="text"
+    />
+    <button
+      @click="todoCreate"
+      class="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 transition ease-in-out text-white"
+    >
+      ADD
+    </button>
+  </div>
+</template>
---
+++ src/components/todo/Todos.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import Heading1 from "../typography/Heading1.vue";
+import TodoItem from "./TodoItem.vue";
+import TodoCreate from "./TodoCreate.vue";
+let todos = ref([]);
+const save = () => {
+  localStorage.setItem("tailwindtodos", JSON.stringify(todos.value));
+};
+const get = () => {
+  const store = JSON.parse(localStorage.getItem("tailwindtodos"));
+  console.log(store);
+  todos.value = store;
+};
+onMounted(() => {
+  get();
+});
+const todoCreate = (todo) => {
+  todos.value = [...todos.value, todo];
+  save();
+};
+const todoRemove = (index) => {
+  todos.value.splice(index, 1);
+  save();
+};
+const todoToggle = (index) => {
+  save();
+};
+const todosClearCompleted = () => {
+  todos.value = [
+    ...todos.value.filter((todo) => {
+      return !todo.completed;
+    }),
+  ];
+  save();
+};
+</script>
+
+<template>
+  <div class="mx-auto">
+    <div class="text-center">
+      <Heading1>Just another todo app</Heading1>
+    </div>
+    <div class="shadow-lg mt-8 max-w-lg mx-auto">
+      <TodoCreate @todoCreate="todoCreate" />
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :todo="todo"
+        :index="index"
+        @todoRemove="todoRemove"
+        @todoToggle="todoToggle"
+      ></TodoItem>
+    </div>
+    <div class="mt-8 max-w-lg mx-auto">
+      <button
+        @click="todosClearCompleted"
+        class="px-8 py-2 bg-red-500 text-white hover:bg-red-600"
+      >
+        Clear completed
+      </button>
+    </div>
+  </div>
+</template>
